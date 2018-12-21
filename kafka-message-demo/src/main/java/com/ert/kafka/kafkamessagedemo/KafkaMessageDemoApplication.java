5a48b681fcc0c7d2a2b239b2107d797f48cacecf
ertanngul <ertann.gul@gmail.com>
2018-12-21 12:19:59 +0100

    first upload
    first upload

---
+++ kafka-message-demo/src/main/java/com/ert/kafka/kafkamessagedemo/KafkaMessageDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ert.kafka.kafkamessagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageDemoApplication.class, args);
+	}
+
+}
+
---
+++ kafka-message-demo/src/test/java/com/ert/kafka/kafkamessagedemo/KafkaMessageDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ert.kafka.kafkamessagedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaMessageDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
